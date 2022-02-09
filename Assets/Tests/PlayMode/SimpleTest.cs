1effb68dec913828fdaf6cad4196799b38a25914
Álvaro Ramírez <4lvaro12amirez@gmail.com>
2022-02-09 16:31:07 +0100

    Proyecto de Unity

---
+++ Assets/Tests/PlayMode/SimpleTest.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class SimpleTest
+{
+    [Test]
+    public void SimpleTestSimplePasses()
+    {
+        Assert.AreEqual(2 + 2, 4);
+    }
+}
