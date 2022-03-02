6335549dc2ed1a6b9719045a66c6782ad249f1c2
Álvaro Ramírez <4lvaro12amirez@gmail.com>
2022-03-02 17:34:05 +0100

    Delete Assets directory

--- Assets/Scripts/Fish/FishBaseState.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public abstract class FishBaseState 
-{
-    public abstract void EnterState(FishStateManager fish);
-    public abstract void UpdateState(FishStateManager fish);
-    public abstract void OnCollisionEnter(FishStateManager fish, Collision collision);
-}
--- Assets/Scripts/Fish/FishBitingState.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class FishBitingState : FishBaseState
-{
-    public override void EnterState(FishStateManager fish) { }
-    public override void UpdateState(FishStateManager fish) { }
-    public override void OnCollisionEnter(FishStateManager fish, Collision collision) { }
-}
