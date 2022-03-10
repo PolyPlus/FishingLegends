b9e89ba681903ad6743a46b352dc1cd0f534dbe3
martagp14 <57727166+martagp14@users.noreply.github.com>
2022-03-10 23:10:02 +0100

    Creado GameManager

---
+++ Fishing Legends/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    public static void LoadMain()
+    {
+        GameObject main = GameObject.Instantiate(Resources.Load("GameManager")) as GameObject;
+        GameObject.DontDestroyOnLoad(main);
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+
+    public static GameManager GetInstance()
+    {
+        return instance;
+    }
+
+    public void SelectScene(string nameScene)
+    {
+        SceneManager.LoadScene(nameScene);
+    }
+}
