578abac2514537ef802c10fe775a07209870ba5d
AlvaroGA99 <55707621+AlvaroGA99@users.noreply.github.com>
2022-03-23 20:16:10 +0100

    Cielo y Transición

--- Fishing Legends/Assets/Scripts/CircleFade.cs
+++ Fishing Legends/Assets/Scripts/CircleFade.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class CircleFade : MonoBehaviour
 {
+    bool isActive = true;
+
+    public bool isStarting = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,35 @@ public class CircleFade : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isActive)
+        {
+            if (isStarting)
+            {   if(transform.localScale.x <= 100)
+                {
+
+                    transform.localScale += new Vector3(100, 100, 100) * Time.deltaTime;
+                    //transform.Rotate(new Vector3(0, 0, 75 * Time.deltaTime));
+                }
+                else
+                {
+                    //isActive = false;
+                }
+            }
+            else
+            {
+                if (transform.localScale.x > 0)
+                {
+                    transform.localScale -= new Vector3(100, 100, 100)*Time.deltaTime;
+                    //transform.Rotate(new Vector3(0, 0, 75* Time.deltaTime));
+                }
+                else
+                {
+                    //isActive = false;
+                    transform.localScale = new Vector3(0, 0, 0);
+                }
+                    
+            }
+            
+        }
     }
 }
