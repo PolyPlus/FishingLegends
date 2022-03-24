213a42a76d52b404e0fa3a122ce273622b32f1bc
anamarq <anamarq98@uma.es>
2022-03-24 11:38:28 +0100

    Actualizando canvas pscipedia

--- Fishing Legends/Assets/Scripts/CanvasTiendaRanking/ScriptPscipedia.cs
+++ Fishing Legends/Assets/Scripts/CanvasTiendaRanking/ScriptPscipedia.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class ScriptPscipedia : MonoBehaviour
 {
+    public Sprite newImage;
+    public bool change = false;
+    public int fishID=1;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,28 @@ public class ScriptPscipedia : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (change)
+        {
+            transform.GetChild(fishID).GetComponent<Image>().sprite = newImage;
+            //switch (fishID) 
+            //{
+            //    case 1:
+            //        transform.GetChild(0).GetComponent<Image>().sprite = newImage;
+            //        break;
+            //    case 2:
+            //        transform.GetChild(1).GetComponent<Image>().sprite = newImage;
+            //        break;
+            //    default:
+            //        break;
+            //} 
+
+
+            //  GetComponent<Image>().sprite = newImage;
+        }
+    }
+    void changeImage()
+    {
+        if(change)
+        GetComponent<Image>().sprite=newImage;
     }
 }
