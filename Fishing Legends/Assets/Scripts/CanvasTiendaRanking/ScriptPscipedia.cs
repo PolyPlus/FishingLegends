2c33a2a44c234824e70a2fc5ff4d74681c901c7f
anamarq <anamarq98@uma.es>
2022-03-28 16:39:13 +0200

    Finalizado canvas psipedia y subido los sprites de pez

--- Fishing Legends/Assets/Scripts/CanvasTiendaRanking/ScriptPscipedia.cs
+++ Fishing Legends/Assets/Scripts/CanvasTiendaRanking/ScriptPscipedia.cs
@@ -4,40 +4,39 @@ using UnityEngine;
 using UnityEngine.UI;
 public class ScriptPscipedia : MonoBehaviour
 {
-    public Sprite newImage;
-    public bool change = false;
-    public int fishID=1;
+    public Sprite octopus;
+    public Sprite seaHorse;
+    public Sprite dangerousFish;
+    public Sprite clownfish;
+    public Sprite star;
+    public Sprite sharp;
+    public Sprite swordfish;
+    public Sprite kiteFish;
+    public Sprite barracuda;
+    public Sprite surgeonFish;
+    public Sprite crab;
+    public Sprite stingray;
+    public Sprite blowFish;
+
+   // public bool change = false;
+   // public int fishID=3;
+    private Sprite[] fishes;
     // Start is called before the first frame update
     void Start()
     {
-        
+        fishes = new Sprite[] { barracuda,seaHorse, crab,surgeonFish,star,stingray,kiteFish,swordfish,blowFish, clownfish,dangerousFish,octopus, sharp };
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (change)
-        {
-            transform.GetChild(fishID).GetComponent<Image>().sprite = newImage;
-            //switch (fishID) 
-            //{
-            //    case 1:
-            //        transform.GetChild(0).GetComponent<Image>().sprite = newImage;
-            //        break;
-            //    case 2:
-            //        transform.GetChild(1).GetComponent<Image>().sprite = newImage;
-            //        break;
-            //    default:
-            //        break;
-            //} 
-
-
-            //  GetComponent<Image>().sprite = newImage;
-        }
+       
     }
-    void changeImage()
+    void changeImage(int fishID)
     {
-        if(change)
-        GetComponent<Image>().sprite=newImage;
+        if ((fishID >=0) && (fishID < fishes.Length))
+        {
+            transform.GetChild(fishID).GetComponent<Image>().sprite = fishes[fishID];
+        }
     }
 }
