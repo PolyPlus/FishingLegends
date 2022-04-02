ba8a10af7e159815ff4b47d57486805141ecd78a
oihanion <oihanion@gmail.com>
2022-04-03 01:12:59 +0200

    Actualizar maxAnzuelos

--- Fishing Legends/Assets/Scripts/StaticInfo.cs
+++ Fishing Legends/Assets/Scripts/StaticInfo.cs
@@ -16,7 +16,7 @@ public static class StaticInfo
 
     public static LinkedList<Vector3> indexPoints;
     // Guardar en playerPrefs
-    public static int monedas, nivelBarco = 1, maxAnzuelos = 2;
+    public static int monedas, nivelBarco = 1, maxAnzuelos = 5;
     // Actualizar en cada ruta
     public static int fishingScore, totalScore, numAnzuelos;
 
