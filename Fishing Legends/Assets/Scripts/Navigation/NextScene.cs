9082c087ec52c6b48eb91d894c8fab6b59e8562f
martagp14 <57727166+martagp14@users.noreply.github.com>
2022-04-03 15:48:43 +0200

    Transicion de sonido desde navegacion

--- Fishing Legends/Assets/Scripts/Navigation/NextScene.cs
+++ Fishing Legends/Assets/Scripts/Navigation/NextScene.cs
@@ -5,12 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class NextScene : StateMachineBehaviour
 {
-    
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        AudioManager.instance.FadeMusic(true);
+    }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
