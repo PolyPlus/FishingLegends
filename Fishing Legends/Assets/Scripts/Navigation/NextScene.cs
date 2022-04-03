e38f8d6c0f34fc8af3fd8fe433813ffdbf7d3291
AlvaroGA99 <55707621+AlvaroGA99@users.noreply.github.com>
2022-04-03 13:44:25 +0200

    Retoques Mapa

--- Fishing Legends/Assets/Scripts/Navigation/NextScene.cs
+++ Fishing Legends/Assets/Scripts/Navigation/NextScene.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class NextScene : StateMachineBehaviour
 {
+    
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     //{
