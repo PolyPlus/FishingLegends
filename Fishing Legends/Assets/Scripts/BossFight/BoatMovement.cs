421c90910f363b2ceef2b20464f6425dafbc44c8
Álvaro Ramírez <4lvaro12amirez@gmail.com>
2022-04-13 21:48:48 +0200

    Más peces añadidos e ids actualizados

---
+++ Fishing Legends/Assets/Scripts/BossFight/BoatMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatMovement : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody rb;
+
+    float horizontalInput;
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
