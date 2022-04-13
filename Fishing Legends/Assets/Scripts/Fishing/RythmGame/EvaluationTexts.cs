e2da7cd85011c638e33f2875db306d614791d791
martagp14 <57727166+martagp14@users.noreply.github.com>
2022-04-13 21:31:54 +0200

    Textos que evaluan el toque en el minijuego de pesca

---
+++ Fishing Legends/Assets/Scripts/Fishing/RythmGame/EvaluationTexts.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EvaluationTexts : MonoBehaviour
+{
+    public Text ExcelenteText;
+    public Text BienText;
+    public Text MalText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HideEvaluationtext();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void showEvText(int text)
+    {
+        switch (text)
+        {
+            case 0:
+                StartCoroutine(showEvaluationText(ExcelenteText));
+                break;
+            case 1:
+                StartCoroutine(showEvaluationText(BienText));
+                break;
+            case 2:
+                StartCoroutine(showEvaluationText(MalText));
+                break;
+        }
+    }
+
+    private IEnumerator showEvaluationText(Text text)
+    {
+        Color c = text.color;
+        c.a = 1;
+        text.color = c;
+        yield return new WaitForSeconds(0.2f);
+        while (text.color.a > 0)
+        {
+            c = text.color;
+            c.a = c.a - 0.02f;
+            text.color = c;
+            Debug.Log("Alfa"+text.color.a);
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    private void HideEvaluationtext()
+    {
+        Color c = ExcelenteText.color;
+        c.a = 0;
+        ExcelenteText.color = c;
+        c = ExcelenteText.color;
+        c.a = 0;
+        ExcelenteText.color = c;
+        c = ExcelenteText.color;
+        c.a = 0;
+        ExcelenteText.color = c;
+    }
+}
